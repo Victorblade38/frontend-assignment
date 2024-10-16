64f105f583f692c50c694ff69dbcfb3fc4048a7b
prathammhavale <prathammhavale38@gmail.com>
2024-10-16 20:23:38 +0530

    done

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    rotate: {
+      360: "360deg",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
